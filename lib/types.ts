edd9299c576377d7fc693427e225ded627f1a248
Gusscar <gusscarmona@gmail.com>
2026-05-07 10:24:48 -0400

    fix: exclude context from PhraseInsert type

--- lib/types.ts
+++ lib/types.ts
@@ -16,5 +16,5 @@ export interface Phrase {
 
 export type PhraseInsert = Omit<
   Phrase,
-  "id" | "created_at" | "correct_count" | "incorrect_count" | "interval" | "ease_factor" | "repetitions" | "next_review_date"
+  "id" | "created_at" | "correct_count" | "incorrect_count" | "interval" | "ease_factor" | "repetitions" | "next_review_date" | "context"
 >;
